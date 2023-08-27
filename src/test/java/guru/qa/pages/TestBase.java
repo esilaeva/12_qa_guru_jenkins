5895e6a5b638a7fd74d17fb4e4b64e5de8ffec52
esilaeva <emsilaeva@gmail.com>
2023-08-27 21:50:41 +0300

    update testBase

--- src/test/java/guru/qa/pages/TestBase.java
+++ src/test/java/guru/qa/pages/TestBase.java
@@ -19,10 +19,12 @@ public class TestBase {
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+        capabilities.setCapability("selenoid:options", Map.of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @AfterEach
