74e5afb6b82b78b9a22b5809777770a1f5bb111f
esilaeva <emsilaeva@gmail.com>
2023-08-29 14:02:55 +0300

    update

--- src/test/java/guru/qa/tests/RegistrationPageObjectsWithFakerTests.java
+++ src/test/java/guru/qa/tests/RegistrationPageObjectsWithFakerTests.java
@@ -3,12 +3,27 @@ package guru.qa.tests;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import guru.qa.utils.WebSteps;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationPageObjectsWithFakerTests extends TestBase {
 
+    @Tag("smoke")
+    @Tag("all")
     @Test
     void successRegistrationFullTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.deleteBanners();
+        steps.fillForm();
+        steps.checkResult();
+    }
+
+    @Tag("all")
+    @Test
+    void successRegistrationTest() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         WebSteps steps = new WebSteps();
 
